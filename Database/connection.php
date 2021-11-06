7bde9b86def9fcd3a6a7c41e6ba37e71a6efa360
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2021-11-06 11:35:32 +0800

    initial

---
+++ Database/connection.php
@@ -0,0 +1,10 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE_NAME", "bulsu_one");
+
+$conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME);
+if($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/master.css">
+</head>
+
+<body>
+
+    <nav>
+        <div class="logo">
+            <a href="index.php"><img class="img-logo" src="images/logo.png"></a>
+        </div>
+
+        <ul>
+            <li class="active-menu">
+                <a href="views/route">Routes</a>
+            </li>
+            <li>
+                <a href="views/ship">Ships</a>
+            </li>
+            <li>
+                <a href="views/crew">Crews</a>
+            </li>
+        </ul>
+    </nav>
+
+</body>
+
+</html>
